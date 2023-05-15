da270af2ef60532f15c9b3e1b3c5c3357cd74404
CoderFrancis <mfcollegeacc@gmail.com>
2023-05-16 01:43:23 +0530

    Create StringManipulation.cs

---
+++ StringManipulation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Concatenating three strings
+            string string1 = "Hello";
+            string string2 = " ";
+            string string3 = "world!";
+            string concatenatedString = string1 + string2 + string3;
+
+            Console.WriteLine("Concatenated string: " + concatenatedString);
+            Console.WriteLine();
+
+            // Converting a string to uppercase
+            string lowercaseString = "hello world!";
+            string uppercaseString = lowercaseString.ToUpper();
+
+            Console.WriteLine("Uppercase string: " + uppercaseString);
+            Console.WriteLine();
+
+            // Creating a StringBuilder and building a paragraph of text
+            StringBuilder paragraphBuilder = new StringBuilder();
+
+            paragraphBuilder.Append("This is the first sentence. ");
+            paragraphBuilder.Append("This is the second sentence. ");
+            paragraphBuilder.Append("This is the third sentence.");
+
+            string paragraph = paragraphBuilder.ToString();
+
+            Console.WriteLine("Paragraph: " + paragraph);
+
+            // Wait for user input before closing the console window
+            Console.ReadLine();
+        }
+    }
+}
+
