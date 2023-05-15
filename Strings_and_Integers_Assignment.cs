a8b398b1c650bb6885262ba5002eac90b2e962b7
CoderFrancis <mfcollegeacc@gmail.com>
2023-05-16 03:40:46 +0530

    Create Strings_and_Integers_Assignment.cs

---
+++ Strings_and_Integers_Assignment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DivideList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> numbers = new List<int> { 10, 20, 30, 40, 50 };
+
+                // Ask the user for a number to divide each number in the list by
+                Console.Write("Enter a number to divide each number in the list: ");
+                int divisor = int.Parse(Console.ReadLine());
+
+                // Create a loop to divide each integer in the list and display the result
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    int result = numbers[i] / divisor;
+                    Console.WriteLine($"Result: {result}");
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Error: Division by zero is not allowed.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error: Invalid input. Please enter a valid number.");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program execution continued after the try/catch block.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
