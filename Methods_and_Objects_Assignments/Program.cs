079635a6b12ccefcfb9762b06acf94091248a296
CoderFrancis <mfcollegeacc@gmail.com>
2023-05-18 03:39:14 +0530

    Create Program.cs

---
+++ Methods_and_Objects_Assignments/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace InheritanceExample
+{
+    // Create a base class called Person
+    class Person
+    {
+        // Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Void method to say the person's name
+        public void SayName()
+        {
+            string fullName = $"{FirstName} {LastName}";
+            Console.WriteLine($"Name: {fullName}");
+        }
+    }
+
+    // Create a derived class called Employee, inheriting from Person
+    class Employee : Person
+    {
+        // Property specific to Employee
+        public int Id { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate and initialize an Employee object
+            Employee employee = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student",
+                Id = 1
+            };
+
+            // Call the SayName() method on the Employee object
+            employee.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
+
