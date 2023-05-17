1ab50099690b9c3863fb7507a7d7aab240521c02
CoderFrancis <mfcollegeacc@gmail.com>
2023-05-18 03:52:39 +0530

    Create Program.cs

---
+++ Abstract_Class_Assignments/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AbstractClassExample
+{
+    // Create an abstract class called Person
+    abstract class Person
+    {
+        // Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Abstract method to say the person's name
+        public abstract void SayName();
+    }
+
+    // Create a derived class called Employee, inheriting from Person
+    class Employee : Person
+    {
+        // Implement the SayName() method from the base class
+        public override void SayName()
+        {
+            string fullName = $"{FirstName} {LastName}";
+            Console.WriteLine($"Name: {fullName}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate an Employee object and set its properties
+            Employee employee = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            // Call the SayName() method on the Employee object
+            employee.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
