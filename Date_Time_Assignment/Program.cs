84d9c4001ba498c601ac1c5f1787fe55aae70ece
CoderFrancis <mfcollegeacc@gmail.com>
2023-05-18 05:17:32 +0530

    Create Program.cs

---
+++ Date_Time_Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Print current date and time
+        DateTime currentDateTime = DateTime.Now;
+        Console.WriteLine("Current Date and Time: " + currentDateTime);
+
+        // Ask user for a number
+        Console.Write("Enter a number: ");
+        int hours = Convert.ToInt32(Console.ReadLine());
+
+        // Calculate future date and time
+        DateTime futureDateTime = currentDateTime.AddHours(hours);
+
+        // Print the exact time it will be in X hours
+        Console.WriteLine("In {0} hours, it will be: {1}", hours, futureDateTime);
+
+        // Wait for user to press a key before closing the console
+        Console.ReadKey();
+    }
+}
