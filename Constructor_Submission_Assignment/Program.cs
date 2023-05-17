aa56040c267dc8113f0e06648c89a93c092920ba
CoderFrancis <mfcollegeacc@gmail.com>
2023-05-18 05:24:52 +0530

    Create Program.cs

---
+++ Constructor_Submission_Assignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Create a const variable
+        const int myConst = 10;
+
+        // Create a variable using the keyword "var"
+        var myVar = "Hello, world!";
+
+        // Chain two constructors together
+        MyClass myObject = new MyClass("John");
+        Console.WriteLine(myObject.Name);
+
+        // Wait for user to press a key before closing the console
+        Console.ReadKey();
+    }
+}
+
+class MyClass
+{
+    public string Name { get; set; }
+
+    // Constructor chaining
+    public MyClass() : this("Default Name")
+    {
+    }
+
+    public MyClass(string name)
+    {
+        Name = name;
+    }
+}
