ff32e6dd8eaebe6f1d1a058e61da30ec2543034c
CoderFrancis <mfcollegeacc@gmail.com>
2023-05-18 03:57:25 +0530

    PolyMorphism_Assignment

---
+++ PolyMorphism_Assignments/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace InterfaceExample
+{
+    // Create an interface called IQuittable
+    interface IQuittable
+    {
+        // Define a void method called Quit()
+        void Quit();
+    }
+
+    // Create a class called Employee that inherits from IQuittable
+    class Employee : IQuittable
+    {
+        // Implement the Quit() method from the interface
+        public void Quit()
+        {
+            Console.WriteLine("Employee has quit the job.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of type IQuittable using polymorphism
+            IQuittable quittable = new Employee();
+
+            // Call the Quit() method on the object
+            quittable.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
