d15dba91c9e3e1666ff4ddd9c7843f8991d13c11
CoderFrancis <mfcollegeacc@gmail.com>
2023-05-18 01:01:46 +0530

    CallingMethodAssignment

---
+++ Calling_Method_Assignment/Calling_Method_Assignment.cs
@@ -0,0 +1,25 @@
+﻿using CallingMethods;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int input = int.Parse(Console.ReadLine());
+
+        MathOperations math = new MathOperations();
+
+        // Call each method and display the returned integer
+        int doubledNumber = math.DoubleNumber(input);
+        Console.WriteLine($"Double: {doubledNumber}");
+
+        int squaredNumber = math.SquareNumber(input);
+        Console.WriteLine($"Square: {squaredNumber}");
+
+        int factorialNumber = math.FactorialNumber(input);
+        Console.WriteLine($"Factorial: {factorialNumber}");
+
+        Console.ReadLine();
+    }
+}
---
+++ Calling_Method_Assignment/MathOperations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class MathOperations
+    {
+        // Method that doubles the input number
+        public int DoubleNumber(int number)
+        {
+            return number * 2;
+        }
+
+        // Method that squares the input number
+        public int SquareNumber(int number)
+        {
+            return number * number;
+        }
+
+        // Method that calculates the factorial of the input number
+        public int FactorialNumber(int number)
+        {
+            int result = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+
+}
