4154b327f2dad6026bd83aa9009d37fc6d2d049a
CoderFrancis <mfcollegeacc@gmail.com>
2023-05-18 04:04:39 +0530

    Operators_Assignment_Submission

---
+++ Operators_Assignment/Employee.cs
@@ -0,0 +1,24 @@
+﻿namespace EmployeeComparison
+{
+    // Create an Employee class with properties: Id, FirstName, and LastName
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Overload the "==" operator to compare Employee objects based on their Id property
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            // Check if the Id values of the two employees are equal
+            return emp1.Id == emp2.Id;
+        }
+
+        // Overload the "!=" operator to compare Employee objects based on their Id property
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            // Check if the Id values of the two employees are not equal
+            return emp1.Id != emp2.Id;
+        }
+    }
+}
---
+++ Operators_Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmployeeComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate two Employee objects and assign values to their properties
+            Employee emp1 = new Employee { Id = 1, FirstName = "John", LastName = "Doe" };
+            Employee emp2 = new Employee { Id = 2, FirstName = "Jane", LastName = "Smith" };
+
+            // Compare the two Employee objects using the overloaded "==" and "!=" operators
+            bool areEqual = emp1 == emp2;
+            bool areNotEqual = emp1 != emp2;
+
+            // Display the comparison results
+            Console.WriteLine("Are emp1 and emp2 equal? " + areEqual);
+            Console.WriteLine("Are emp1 and emp2 not equal? " + areNotEqual);
+
+            Console.ReadLine();
+        }
+    }
+}
